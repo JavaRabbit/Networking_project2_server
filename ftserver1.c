319c00ba97dfcd8b32eb25b0e3c0e2d30c4e7c79
Bonnie <kwongb@flip3.engr.oregonstate.edu>
2017-05-26 01:15:48 -0700

    Added validate 2 paramers

--- ftserver1.c
+++ ftserver1.c
@@ -2,8 +2,38 @@
 #include <stdlib.h>
 #include <string.h>
 
+
+/* method prototypes */
+void validateParameters(int, char* []);
+
+/* Function to validate that user 
+ * entered valid port number.
+ * If valid port number is entered, 
+ * function completes and returns to main,
+ * else, usage is displayed to user
+ *  */
+
+void validateParameters(int argc, char *argv[]){
+
+  // if command line does not have 2 arguments
+  // show usage to usr
+  if(argc != 2){
+   printf("Usage example: ./ftserver <portNumber>\n");
+   exit(1);
+  }
+}
+
+
+
+
+
+
+
+
+
 int main(int argc, char * argv[]){
 
   printf("Hello\n");
-
+  validateParameters(argc, argv);
+  printf("After validate params\n");
 }
