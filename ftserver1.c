e154a5a73323332868435062fbd72235906a4ec2
Bonnie <kwongb@flip1.engr.oregonstate.edu>
2017-05-26 18:23:19 -0700

    working on server while loops

--- ftserver1.c
+++ ftserver1.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <errno.h>
+#include <netdb.h>
+
 
 
 /* method prototypes */
@@ -36,4 +43,52 @@ int main(int argc, char * argv[]){
   printf("Hello\n");
   validateParameters(argc, argv);
   printf("After validate params\n");
-}
+
+  // file descriptors to be used
+  int sock_fd, connection_fd, client;
+
+  // struct to hold IP address and port numbers
+  struct sockaddr_in server, client_addr;
+  int portNumber;
+
+  // assign port number to argv[1]
+  portNumber = atoi(argv[1]);
+
+  // Call to Socket function
+  if((sock_fd = socket(AF_INET, SOCK_STREAM, 0)) == -1){
+    perror("socket: ");
+  }
+
+  // clear socket
+  bzero(&server, sizeof(server));
+
+  // set addressing scheme to AF_INET
+  server.sin_family = AF_INET;
+
+  // Listen of specified port
+  server.sin_port = htons(portNumber);
+
+  // Allow any IP to connect
+  server.sin_addr.s_addr = htons(INADDR_ANY);
+
+  // Bind the host address
+  bind(sock_fd, (struct sockaddr *) &server, sizeof(server));
+ 
+  while(1){
+    // start to listen, 
+    listen(sock_fd,5);
+    printf("Listening for a new connection...\n");
+
+    client = sizeof(client_addr);
+
+    while(1){
+      connection_fd = accept(sock_fd, (struct sockaddr *) &client_addr, &client);
+
+      if(connection_fd < 0){
+        fprintf(stderr, "error on accept\n");
+        exit(1);
+      }
+    } // end while loop
+  } // end while loop
+
+}  // end main
